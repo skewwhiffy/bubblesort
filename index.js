1e6bc4e3430645ca57fd25ff7f2829d11dfa773c
Kenny Hung (he) <khung@instructors.2u.com>
2022-07-30 12:29:39 +0100

    Final implementation

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const bubblesort = function(array) {
   if (array.length < 2) {
     return array
   }
-  if (array[0] > array[1]) {
-    return [array[1], array[0]]
+  let elementsHaveBeenSwapped = false
+  for (let i = 0; i < array.length - 1; i++) {
+    const firstElement = array[i]
+    const secondElement = array[i + 1]
+    if (firstElement > secondElement) {
+      array[i + 1] = firstElement
+      array[i] = secondElement
+      elementsHaveBeenSwapped = true
+    }
+  }
+  if (elementsHaveBeenSwapped) {
+    return bubblesort(array)
   }
   return array
 }
