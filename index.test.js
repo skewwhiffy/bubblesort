b3ee9fabc3e166dfa08ee85fa5b3f309637ef869
Kenny Hung (he) <khung@instructors.2u.com>
2022-07-30 12:19:32 +0100

    Sorted array, failing test

--- index.test.js
+++ index.test.js
@@ -4,4 +4,12 @@ describe('dummy test', () => {
   test('bubblesort is a function', () => {
     expect(typeof bubblesort).toBe('function')
   })
+
+  test('sort works with sorted array', () => {
+    const source = [1, 2, 3]
+
+    const result = bubblesort(source)
+
+    expect(result).toBe(source)
+  })
 })
